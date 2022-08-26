8d1f139b43b0a2655fad6ff5c467576f52daa5aa
Dan Jutan <danjutan@gmail.com>
2022-08-26 11:21:06 -0400

    use unused variable as intented

--- packages/create-astro/src/index.ts
+++ packages/create-astro/src/index.ts
@@ -118,7 +118,7 @@ export async function main() {
 
 	// Don't touch the template name if a GitHub repo was provided, ex: `--template cassidoo/shopify-react-astro`
 	const isThirdParty = options.template.includes('/');
-	const templateTarget = options.template.includes('/')
+	const templateTarget = isThirdParty
 		? options.template
 		: `withastro/astro/examples/${options.template}#latest`;
 
